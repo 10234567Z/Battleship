90134d6551812fdbc5f3b3aad8ca592f1c4117b5
10234567Z <93607971+10234567Z@users.noreply.github.com>
2023-10-11 16:12:31 +0530

    Add Ship factory which manages the ship's present condition

--- src/battleship.js
+++ src/battleship.js
@@ -1,1 +1,19 @@
 import './styles.css';
+
+export default function Ship(shipLength) {
+  const ships = {
+    length: shipLength,
+    gotHit: 0,
+    sunk: false,
+
+    hit: function () {
+      this.gotHit++;
+    },
+
+    isSunk: function () {
+      this.length === this.gotHit ? this.sunk = true : this.sunk = false
+    },
+  };
+
+  return { ships };
+}
