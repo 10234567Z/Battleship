23a1c547635e631a0c12e30685132c17fbdaecc3
10234567Z <93607971+10234567Z@users.noreply.github.com>
2023-10-13 15:05:13 +0530

    Add a backtrack algorithm incase the coordinates of ship collide with another

--- src/gameboard.js
+++ src/gameboard.js
@@ -3,24 +3,52 @@ export default function GameBoard() {
 
     let getBoard = () => board;
 
-    let verticalPlace = (shipLength , row , col) => {
+    let verticalPlace = (shipLength, row, col) => {
+        let tempBackTrack;
         for (let i = 0; i < shipLength; i++) {
-            board[row + i][col] = 1;
+            if (board[row + i][col] === 0) {
+                board[row + i][col] = 1;
+            }
+            else {
+                tempBackTrack = i - 1;
+                backTrackVertical(tempBackTrack, row, col);
+                break;
+            }
         }
     }
 
-    let horizontalPlace = (shipLength , row , col) => {
+    let horizontalPlace = (shipLength, row, col) => {
+        let tempBackTrack;
         for (let i = 0; i < shipLength; i++) {
-            board[row][col + i] = 1;
+            if(board[row][col + i] === 0){
+                board[row][col + i] = 1;
+            }
+            else{
+                tempBackTrack = i - 1;
+                backTrackHorizontal(tempBackTrack , row , col);
+                break;
+            }
         }
     }
 
     let PlaceShip = (row, col, ship, vertical = true) => {
         if (vertical && board[row + ship.ships.length] !== undefined) {
-            verticalPlace(ship.ships.length , row , col)
+            verticalPlace(ship.ships.length, row, col)
         }
         else if (!vertical && board[row][col + ship.ships.length] !== undefined) {
-            horizontalPlace(ship.ships.length , row , col)
+            horizontalPlace(ship.ships.length, row, col)
+        }
+    }
+
+    let backTrackVertical = (index, row, col) => {
+        for (let i = index; i >= 0; i--) {
+            board[row + i][col] = 0;
+        }
+    }
+
+    let backTrackHorizontal = (index, row, col) => {
+        for (let i = index; i >= 0; i--) {
+            board[row][col + i] = 0;
         }
     }
 
